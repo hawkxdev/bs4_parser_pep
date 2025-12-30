e30d2652aca4af0bcf20a991085d9d42da5fa067
Sergey Sokolkin <hawkxdev@gmail.com>
2025-12-30 15:32:16 +0300

    feat(constants): add PEP_URL and EXPECTED_STATUS

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,19 @@
-"""Константы парсера: URL-адреса, пути, форматы."""
+"""Константы парсера: URL-адреса, пути, форматы, статусы PEP."""
 from pathlib import Path
 
 
 MAIN_DOC_URL = 'https://docs.python.org/3/'
-BASE_DIR = Path(__file__).parent
+PEP_URL = 'https://peps.python.org/'
+BASE_DIR = Path(__file__).parent.parent
 DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+
+EXPECTED_STATUS = {
+    'A': ('Active', 'Accepted'),
+    'D': ('Deferred',),
+    'F': ('Final',),
+    'P': ('Provisional',),
+    'R': ('Rejected',),
+    'S': ('Superseded',),
+    'W': ('Withdrawn',),
+    '': ('Draft', 'Active'),
+}
